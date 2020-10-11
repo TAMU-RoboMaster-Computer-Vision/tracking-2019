8c8d28fdc69630986fcae01dfbf469be1153f9d1
Vatsal Mistry <vatsalmistry4796@gmail.com>
2020-10-11 18:08:54 -0500

    tried arrow which fails due to rotation and scale

--- filter.py
+++ filter.py
@@ -1,23 +1,31 @@
 import cv2
 import numpy as np
-from math import sqrt
+from math import sqrt,degrees,acos
 
-# CALCULATE CENTER WITH IMAGE OF R
-# CALCULATE H AND W MAX WITH DISTANCE FROM TARGET MODEL USING INTEL API
+# DO ANGLE BETWEEN VECTORS FOR LIGHT BLUE STICK ONCE HIT
 
 cap = cv2.VideoCapture('rune.mp4')
 counter = 0
 MPx,MPy = 0,0
 small_image = cv2.imread('mask_R.PNG')
+# ax,ay = 0,0
+# arrow = cv2.imread('arrow.PNG')
 while(True):
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
+
     blueLight = np.array([40,170,100])
     blueDark = np.array([255,255,255])
     mask = cv2.inRange(hsv, blueLight, blueDark)
     contours,_ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
     boundingBoxes = []
+
+    # if counter%5 == 0:
+    #     result = cv2.matchTemplate(arrow, frame, cv2.TM_SQDIFF_NORMED)
+    #     mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+    #     ax,ay = mnLoc
+    #     print(ax)
+    #     print(ay)
 
 
     if counter%30 == 0:
@@ -25,12 +33,12 @@ while(True):
         mn,_,mnLoc,_ = cv2.minMaxLoc(result)
         MPx,MPy = mnLoc
     counter+=1
-    
+
     for contour in contours:
         area = cv2.contourArea(contour)
         
         if area> 800:
-            cv2.drawContours(frame,contour,-1,(255,255,255),2)
+            cv2.drawContours(frame,contour,-1,(200,200,200),2)
             x,y,w,h = cv2.boundingRect(contour)
             status = True
             # if h>70 or w>70:
@@ -65,6 +73,39 @@ while(True):
     if averageDistance!=0:
         averageDistance/=len(boundingBoxes)
         averageDistance*=.65
+
+    # white = np.array([199,199,199])
+    # white2 = np.array([201,201,201])
+
+    # mask = cv2.inRange(frame, white, white2)
+    # contours,_ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    # boundingBoxes = []
+    
+
+    # for contour in contours:        
+
+    #     x,y,w,h = cv2.boundingRect(contour)
+    #     cv2.rectangle(mask,(x,y),(x+w,y+h),(75,255,100),5)
+
+    #     area = cv2.contourArea(contour)
+
+    #     for boxIndex in range(len(boundingBoxes)):
+    #         box = boundingBoxes[boxIndex]
+    #         if (box[0]+box[2] >= x and x+w >= box[0]) and (box[1]+box[3] >= y and y+h >= box[1]) and area<box[4]*1.5:
+    #             boundingBoxes.remove(box)
+    #             break
+    # cv2.imshow('mask',mask)
+    # bestBox,angle = None,360
+
+    # arrowToCenter=np.array([ax-MPx,ay-MPy])
+    # for box in boundingBoxes:
+    #     boxToCenter = np.array([box[0]+1/2*box[2]-MPx,box[1]+1/2*box[3]-MPy])
+
+    #     dotProduct = np.dot(arrowToCenter,boxToCenter)
+    #     mag1 = sqrt(np.sum(arrowToCenter**2))
+    #     mag2 = sqrt(np.sum(boxToCenter**2))
+    #     angle2 =  degrees(acos(dotProduct/(mag1*mag2)))
+    #     bestBox,angle = (bestBox,angle) if angle2>angle else (box,angle2)
 
     for box in boundingBoxes:
         if box[5]>=averageDistance:
