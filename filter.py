3eba96dd0f6eef645a3773d82066f99357b8cd02
Vatsal Mistry <vatsalmistry4796@gmail.com>
2020-10-13 00:10:21 -0500

    added arrow outlier check

--- filter.py
+++ filter.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from math import sqrt,degrees,acos
+from math import sqrt,degrees,acos,asin
 
 
 cap = cv2.VideoCapture('rune.mp4')
@@ -26,27 +26,51 @@ while(True):
     mask2 = cv2.inRange(hsv, blueLight, blueDark)
     contours2,_ = cv2.findContours(mask2,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 
-    # go through every small filttered contour and if it is within a certain size, set its x and y value to ax and ay which we will use to calculate angle later
-    for contour in contours2:
+    # go through every small filttered contour and if it is within a certain size, set its x and y value to respective arrays 
+    arrx = []
+    arry = []
+    for contour in contours2:        
         area = cv2.contourArea(contour)
-        
-        if area>15 and area<150:
+        if area> 20 and area<500:
             x,y,w,h = cv2.boundingRect(contour)
             ax,ay = x+w/2,y+h/2
-            break
+            arrx.append(ax-MPx)
+            arry.append(ay-MPy)
+
+
+    arrx = np.array(arrx)
+    arry = np.array(arry)
+    meanx = np.mean(arrx)
+    stdx = np.std(arrx)
+    meany = np.mean(arry)
+    stdy = np.std(arry)
+
+    # filter outliers for arrows and the center point by replacing the point with its respective mean x and y values if more than 2 standard deviations away from the mean
+    for i in range(np.size(arrx)):
+        box = [arrx[i]+MPx,arry[i]+MPy]
+        if(abs(arrx[i] - meanx) > 2 * stdx) or (abs(arry[i] - meany) > 2 * stdy) or (box[0]+10 >= MPx-10 and MPx+10 >= box[0]) and (box[1]+10 >= MPy and MPy+10 >= box[1]):
+            arrx[i] = meanx
+            arry[i] = meany
+
+
+    # set arrow position to the mean position 
+    ax = np.mean(arrx)+MPx 
+    ay = np.mean(arry)+MPy
+
 
     # every 30 frames get the new position of the r image
     if counter%30 == 0:
         result = cv2.matchTemplate(small_image, frame, cv2.TM_SQDIFF_NORMED)
         mn,_,mnLoc,_ = cv2.minMaxLoc(result)
         MPx,MPy = mnLoc
+        MPx+=15
+        MPy+=15
     counter+=1
 
     for contour in contours:
         area = cv2.contourArea(contour)
         
-        
-        if area> 600: # filter contour based on minimum area
+        if area> 500: # filter contour based on minimum area
             x,y,w,h = cv2.boundingRect(contour)
             status = True
 
@@ -80,7 +104,7 @@ while(True):
         averageDistance += box[5]
     if averageDistance!=0:
         averageDistance/=len(boundingBoxes)
-        averageDistance*=.75
+        averageDistance*=.5
 
     # get rid of faulty boxes by checking if their distance is below average distance times a certain bias
     boxes = []
@@ -97,8 +121,16 @@ while(True):
         dotProduct = np.dot(arrowToCenter,boxToCenter)
         mag1 = sqrt(np.sum(arrowToCenter**2))
         mag2 = sqrt(np.sum(boxToCenter**2))
-        angle2 =  degrees(acos(dotProduct/(mag1*mag2)))
-        bestBox,angle = (bestBox,angle) if angle2>angle else (box,angle2)
+        angle2 =  degrees(acos(abs(dotProduct)/(mag1*mag2)))
+
+        font                   = cv2.FONT_HERSHEY_SIMPLEX
+        bottomLeftCornerOfText = box[0]+10,box[1]-20
+        fontScale              = .8
+        fontColor              = (107,20,201)
+        lineType               = 3
+
+        cv2.putText(frame,"Angle: {0:.2f}".format(angle2), (bottomLeftCornerOfText), font, fontScale,fontColor,lineType)
+        bestBox,angle = (bestBox,angle) if (angle2>angle) else (box,angle2)
 
     if bestBox:
         cv2.rectangle(frame,(bestBox[0],bestBox[1]),(bestBox[0]+bestBox[2],bestBox[1]+bestBox[3]),(0,255,0),5)
