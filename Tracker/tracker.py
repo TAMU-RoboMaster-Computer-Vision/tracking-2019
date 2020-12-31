2e540899c9a0f2d540ecd328b06b24712f27c0df
Sai Juttu <saijuttu30@gmail.com>
2020-12-30 19:18:29 -0600

    working no filter depth reader

--- Tracker/tracker.py
+++ Tracker/tracker.py
@@ -2,21 +2,12 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 
-pipeline = rs.pipeline()                                            # declares and initializes the pipeline variable
-config = rs.config()                                                # declares and initializes the config variable for the pipeline
-config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)  # this starts the depth stream and sets the size and format
-config.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 30) # this starts the color stream and set the size and format
-profile = pipeline.start(config)
-
-# wait for five frames so that camera can autoexpose to lighting
-for x in range(5):
-  pipeline.wait_for_frames()
-print("Ready")
+pipeline = rs.pipeline()                                             # declares and initializes the pipeline variable
+pipeline.start()
 
 # This creates a 9 by 9 grid of points within the given bounding box and stores each of the points distance in an array
 def getDistFromArray(depth_frame_array, bbox):
     
-    # bbox[x coordinate of the top left of the bounding box, y coordinate of the top left of the bounding box, width of box, height of box]
     xTopLeft = bbox[0] 
     yTopLeft = bbox[1]
     width = bbox[2]
@@ -46,7 +37,7 @@ def getDistFromArray(depth_frame_array, bbox):
 
     # goes through distances array and adds any values that are less than X*standard deviations and ignores the rest
     for i in range(np.size(distances)):
-        if abs(distances[i] - median) < 2.5 * std: # tune the standard deviation range for better results
+        if abs(distances[i] - median) < 1.5 * std: # tune the standard deviation range for better results
             modifiedDistances = np.append(modifiedDistances,distances[i])
 
     modifiedDistances = np.sort(modifiedDistances)
@@ -69,42 +60,42 @@ try:
         if not depth_frame or not color_frame:  # if there is no aligned_depth_frame or color_frame then leave the loop
             continue
 
-        # SPATIAL FILTER
-        colorizer = rs.colorizer()
-        spatial = rs.spatial_filter()
-        spatial.set_option(rs.option.filter_magnitude, 5)
-        spatial.set_option(rs.option.filter_smooth_alpha, 1)
-        spatial.set_option(rs.option.filter_smooth_delta, 50)
-        spatial.set_option(rs.option.holes_fill, 3)
-        filtered_depth = spatial.process(depth_frame)
-        filtered_image = np.asanyarray(filtered_depth.get_data())
-        colorized_depth = np.asanyarray(colorizer.colorize(filtered_depth).get_data())
-
         # we turn the depth and color frames into numpy arrays because we need to draw a rectangle and stack the two arrays
         depth_image = np.asanyarray(depth_frame.get_data()) # this takes the aligned depth frame and converts the data into a numpy array
-        color_image = np.asanyarray(color_frame.get_data()) # this takes the aligned color frame and converts the data into a numpy array
-        color_image = cv2.rectangle(color_image, (bbox[0],bbox[1]), (bbox[0]+bbox[2],bbox[1]+bbox[3]),(255, 0, 0), 5) # this draws a bounding box to the color frame
+        # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha = 0.04), cv2.COLORMAP_JET)# this puts a color efffect on the depth frame
+        # color_image = np.asanyarray(color_frame.get_data()) # this takes the aligned color frame and converts the data into a numpy array
+        # color_image = cv2.rectangle(color_image, (bbox[0],bbox[1]), (bbox[0]+bbox[2],bbox[1]+bbox[3]),(255, 0, 0), 5) # this draws a bounding box to the color frame
 
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha = 0.04), cv2.COLORMAP_JET)        # this puts a color efffect on the depth frame
-        filtered_colormap = cv2.applyColorMap(cv2.convertScaleAbs(filtered_image, alpha = 0.04), cv2.COLORMAP_JET)  # this puts a color effect on the filtered depth frame
-
-        # images = np.hstack((color_image, depth_colormap)) # this stacks the color image and the depth image next to each other
-        images = colorized_depth                           # this shows the colorized filter image in with black deadspots
-        # images = np.hstack((depth_colormap, filtered_colormap)) # this stacks the depth image and the filtered depth image next to each other
-        cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
-        cv2.imshow('Align Example', images)
-        key = cv2.waitKey(1)
+        # SPATIAL FILTER
+        # colorizer = rs.colorizer()
+        # spatial = rs.spatial_filter()
+        # spatial.set_option(rs.option.filter_magnitude, 1) # values 1-5 for intensity
+        # spatial.set_option(rs.option.filter_smooth_alpha, 1) # values .25-1 for intensity
+        # spatial.set_option(rs.option.filter_smooth_delta, 50) # values 1-50 for intensity
+        # spatial.set_option(rs.option.holes_fill, 1) # values 1-5 for intensity
+        # filtered_depth = spatial.process(depth_frame)
+        # filtered_image = np.asanyarray(filtered_depth.get_data())
+        # colorized_depth = np.asanyarray(colorizer.colorize(filtered_depth).get_data())
+        # filtered_colormap = cv2.applyColorMap(cv2.convertScaleAbs(filtered_image, alpha = 0.04), cv2.COLORMAP_JET)  # this puts a color effect on the filtered depth frame
+        
+        # you can show: color_image, depth_colormap, filtered_colormap, colorized_depth
+        # images = np.hstack((color_image, depth_colormap))     # use this if you want to compare streams
+        # images = depth_colormap                              # use this for individual streams
+        # cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)   # names and shows the streams
+        # cv2.imshow('Align Example', images)
+        # # if you press escape or q you can cancel the process
+        # key = cv2.waitKey(1)
+        # print("press escape to cancel")
+        # if key & 0xFF == ord('q') or key == 27:
+        #     cv2.destroyAllWindows()
+        #     break
 
         getDistFromArray(depth_image, bbox)     # gets the distance for the normal depth image
-        getDistFromArray(filtered_image, bbox)  # gets the distance for the filtered depth image
-        # value += 1 
-        # print(value)
+        # getDistFromArray(filtered_image, bbox)  # gets the distance for the filtered depth image
+        value += 1 
+        print(value)
+      
 
-        # if you press escape or q you can cancel the process
-        print("press escape to cancel")
-        if key & 0xFF == ord('q') or key == 27:
-            cv2.destroyAllWindows()
-            break
 
 finally:
     pipeline.stop()
